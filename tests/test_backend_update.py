37f337bce82c2e9b15b1cde6a4039d7ad9c5cec5
Kai Biebel <38378574+seclution@users.noreply.github.com>
2025-06-20 00:05:40 +0200

    Split imports and statements in tests

--- tests/test_backend_update.py
+++ tests/test_backend_update.py
@@ -1,4 +1,7 @@
-import os, sys; sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from backend import app as backend_app
 import json
 
--- tests/test_client_update_dns.py
+++ tests/test_client_update_dns.py
@@ -1,4 +1,7 @@
-import os, sys; sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from client import update_dns
 import pytest
 import requests
